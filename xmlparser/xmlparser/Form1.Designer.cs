339e632ca60a763a9647d52d7415dcb452b4360c
Joel Magnetti <jmagnetti@gmail.com>
2011-07-04 09:38:40 +1000

    Add XMLParser application

---
+++ xmlparser/xmlparser/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace xmlparser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(36, 62);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(309, 199);
+            this.checkedListBox1.TabIndex = 6;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(222, 280);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 44);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Go";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(42, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 341);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "XML Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ xmlparser/xmlparser/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace xmlparser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            checkedListBox1.Items.Clear();
+            List<string> attributes = new List<string>();
+            openFileDialog1.Filter = "XML files (*.xml)|*.xml";
+            DialogResult dr = openFileDialog1.ShowDialog(this);
+            if (dr == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                System.Xml.XmlTextReader reader = new System.Xml.XmlTextReader(openFileDialog1.FileName);
+
+                while (attributes.Count == 0 && !reader.EOF)
+                {
+                    reader.Read();
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: // The node is an element.
+                            Console.Write("<" + reader.Name);
+
+                            while (reader.MoveToNextAttribute()) // Read the attributes.
+                            {
+                                attributes.Add(reader.Name);
+                            }
+                            Console.WriteLine(">");
+                            break;
+
+                    }
+                    checkedListBox1.Items.AddRange(attributes.ToArray());
+                }
+                reader.Close();
+                label1.Text = "Please select 2 items";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count == 2)
+            {
+                string att1 = "", att2 = "";
+                List<KeyValuePair<string, string>> itemsToOutput = new List<KeyValuePair<string, string>>();
+                System.Xml.XmlTextReader reader = new System.Xml.XmlTextReader(textBox1.Text);
+                List<string> attributesToGet = new List<string>();
+                StringBuilder sb = new StringBuilder();
+                foreach (string item in checkedListBox1.CheckedItems)
+                {
+                    attributesToGet.Add(item);
+                }
+                while (!reader.EOF)
+                {
+                    reader.Read();
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: // The node is an element.
+                            Console.Write("<" + reader.Name);
+                            while (reader.MoveToNextAttribute()) // Read the attributes.
+                            {
+                                if (attributesToGet.Contains(reader.Name))
+                                {
+                                    if (att1 == "") att1 = reader.Value; else att2 = reader.Value;
+                                }
+                            }
+                            if (att1 != "")
+                            {
+                                sb.AppendLine(att1 + "," + att2);
+                                att1 = "";
+                                att2 = "";
+                            }
+                            
+                            Console.WriteLine(">");
+                            break;
+
+                    }
+                }
+                saveFileDialog1.Filter = "TXT files (*.txt)|*.txt";
+                DialogResult dr = saveFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    StreamWriter writer = new StreamWriter(saveFileDialog1.FileName);
+                    writer.Write(sb.ToString());
+                    writer.Close();
+                    MessageBox.Show("File " + saveFileDialog1.FileName + " created successfully.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select 2 attribute types.", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count + (e.NewValue == CheckState.Checked ? 1 : -1) == 2)
+            {
+                label1.Text = "";
+                button5.Enabled = true;
+                
+            }
+            else
+            {
+                label1.Text = "Please select 2 items";
+                button5.Enabled = false;
+            }
+        }
+    }
+}
